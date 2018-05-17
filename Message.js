b5ffb800e0c0caabcadc09dfc01fa74ef5af0535
dsengmanivanh <daniel.sengmanivanh@pole-emploi.fr>
2018-05-17 14:52:42 +0200

    first message

--- Message.js
+++ Message.js
@@ -1,8 +1,20 @@
+import request from 'request';
+
+
 class Message {
 
   // Handles messages events
   handleMessage(sender_psid, received_message) {
-
+    let response;
+    // Check if the message contains text
+    if (received_message.text) {
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+    }
+    // Sends the response message
+    callSendAPI(sender_psid, response);
   }
 
   // Handles messaging_postbacks events
@@ -12,8 +24,26 @@ class Message {
 
   // Sends response messages via the Send API
   callSendAPI(sender_psid, response) {
-
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
+    // Send the HTTP request to the Messenger Platform
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    });
   }
-
 }
 module.exports = Message;
