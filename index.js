c69765da130d11c573d5e6ce079022330f599c05
dsengmanivanh <daniel.sengmanivanh@pole-emploi.fr>
2018-05-17 15:13:56 +0200

    test first message

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/webhook', (req, res) => {
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
-      if (webhook_event.message) {
+      if (webhook_event.messages) {
         console.log('=======================Entre dans message');
         Message.handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
